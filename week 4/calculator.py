30cb581a7d752e767fc7eee7ed0b8d78b008aced
DanielPSparta <you@example.com>
2021-09-07 13:05:35 +0100

    Added in exception handling, and optomised some functions

--- week 4/calculator.py
+++ week 4/calculator.py
@@ -1,48 +1,52 @@
-
 def user():
-    num = float(input("please input a number:"))
-    return num
+    num1 = float(input("please input the first number: "))
+    num2 = float(input("please input the second number: "))
+    return num1, num2
 
 def check(num1,num2):
-    if float(num2) == 0:
-        print("please enter a non zero number")
-    else:
-        print("the addition of the number: ",sum(num1,num2))
-        f.write("{} + {} = {} \n".format(num1,num2,sum(num1,num2)))
-        print("the subtraction of the number: ",minus(num1,num2))
-        f.write("{} - {} = {} \n".format(num1,num2,minus(num1,num2)))
-        print("the multiplication of the number: ",times(num1,num2))
-        f.write("{} * {} = {} \n".format(num1,num2,times(num1,num2)))
-        print("the division of the number: ",divide(num1,num2))
-        f.write("{} / {} = {} \n".format(num1,num2,divide(num1,num2)))
-        f.write("---------------------------------------------------------------\n")
+    print("the addition of the number: ",sum(num1,num2))
+    f.write("{} + {} = {} \n".format(num1,num2,sum(num1,num2)))
 
-def sum(num1,num2):
-    num3 = num1 +num2
-    return num3
+    print("the subtraction of the number: ",minus(num1,num2))
+    f.write("{} - {} = {} \n".format(num1,num2,minus(num1,num2)))
 
+    print("the multiplication of the number: ",times(num1,num2))
+    f.write("{} * {} = {} \n".format(num1,num2,times(num1,num2)))
 
-def minus(num1,num2):
-    num3 = num1 -num2
-    return num3
+    print("the division of the number: ",divide(num1,num2))
+    f.write("{} / {} = {} \n".format(num1,num2,divide(num1,num2)))
 
-def times(num1,num2):
-    num3 = num1 *num2
-    return num3
+    f.write("---------------------------------------------------------------\n")
+
+#operators
+sum = lambda num1, num2 : num1+num2
+minus = lambda num1, num2 : num1-num2
+times = lambda num1, num2 : num1*num2
+
+#division needs exception handling
 
 def divide(num1,num2):
-    num3 = num1 /num2
-    return num3
+    try:
+        num3 = num1 /num2
+    except ZeroDivisionError:
+        print(ZeroDivisionError)
+        return print("Not able to divide by zero")
 
+    return num3
+#set up token for while loop
 
 answer = "n"
+
+#open up a file to write to
 f = open("results.txt","w+")
+
+#runs the code
 while answer != "y":
 
-    number1 = user()
-    number2 = user()
+    number1 ,number2 = user()
     check(number1,number2)
 
 
     answer = input("Are you done? (y/n) \n")
+#closes the file to write to
 f.close()
